0da57beb203f3b88cdfdad427017209f7a85bca0
arunimsamudra <arunim_samudra@tamu.edu>
2024-12-29 17:19:06 +0530

    gcp model inclusion

--- local/server/src/main/config.py
+++ local/server/src/main/config.py
@@ -7,8 +7,8 @@ class Config:
     OPENAI_API_KEY = ''
     DECISION_THRESHOLD = 5000  # Word limit for using local LLM
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    SERVICE_ACCOUNT_FILE_NAME = 'personal-assistant-440203-a23b1a7ea851.json'
     LOCAL_MODEL_PATH = os.path.join(ROOT_DIR, "models/llm")
     CLASSIFIER_MODEL_PATH = os.path.join(ROOT_DIR, "models/classifier/logistic_regression_model.pkl")
-    CLOUD_API_URL = 'https://cloud-6d541cf-v4.app.beam.cloud'
     CPU_THRESHOLD = 70
     MEMORY_THRESHOLD = 3000  # MB
--- local/server/src/main/impl/cloud_llm.py
+++ local/server/src/main/impl/cloud_llm.py
@@ -1,30 +1,57 @@
+import os
+
 from main.config import Config
 from main.impl.llm import LLM
-import requests
 import json
+import requests
+import google.auth
+import google.auth.transport.requests
+from google.oauth2 import service_account
 
 
 class Cloud(LLM):
 
+    def __init__(self):
+        SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
+        src_directory = os.path.abspath(os.path.join(Config.ROOT_DIR, '..'))
+        SERVICE_ACCOUNT_FILE = os.path.join(src_directory, 'resources', Config.SERVICE_ACCOUNT_FILE_NAME)
+        cred = service_account.Credentials.from_service_account_file(
+            SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+        auth_req = google.auth.transport.requests.Request()
+        cred.refresh(auth_req)
+        self.bearer_token = cred.token
+        project_id = "198585961536"
+        endpoint_id = "3797775834501087232"
+        base_url = "https://us-central1-aiplatform.googleapis.com/v1/projects/{project_id}/locations/us-central1/endpoints/{endpoint_id}:predict"
+        self.full_url = base_url.format(project_id=project_id, endpoint_id=endpoint_id)
+
     def summarize_text(self, text):
-        prompt = (
-            "You are a highly intelligent and concise text summarization assistant. "
-            "Your task is to summarize the given text clearly and accurately. "
-            "Keep the summary informative yet brief. Do not output your thought process, just output the summary\n\n"
-            f"Text to be summarized: {text}"
-        )
-        payload = json.dumps({
-            "prompt": prompt
-        })
+        request_body = {
+            "instances": [
+                {
+                    "prompt": "You are a highly intelligent and concise text summarization assistant. "
+                              "Your task is to extract the key points and summarize the given text clearly and accurately. "
+                              "Keep the summary informative yet brief."
+                              f"Text to be summarized: {text}",
+                    "max_tokens": 1000,
+                    "top_k": 1,
+                    "top_p": 1.0,
+                    "temperature": 0.2
+                }
+            ]
+        }
         headers = {
-            'Connection': 'keep-alive',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer 0lXKBW3n6nYYderfIrxE-JeIXjDxKpKBRvOC7Z2anpRuPj_ZTLg8_DJhcCk9-bckksSpWSr1bzf0z_Nw86Klbw=='
+            "Authorization": "Bearer {bearer_token}".format(bearer_token=self.bearer_token),
+            "Content-Type": "application/json"
         }
 
         try:
-            response = requests.post(Config.CLOUD_API_URL, headers=headers, data=payload, timeout=30)
-            return json.loads(response.text)
+            response = requests.post(self.full_url, json=request_body, headers=headers, timeout=30)
+            response = response.json()
+            predictions = response["predictions"]
+            output_text = predictions[0]
+            output_text = output_text.split("Output:", 1)[-1].strip()
+            return json.loads(output_text)
         except requests.exceptions.Timeout:
             return {"error": "The request timed out after 30 seconds."}
         except requests.exceptions.RequestException as e:
@@ -38,18 +65,31 @@ class Cloud(LLM):
             "to answer the question, let the user know by stating: 'The context does not provide enough information to answer this question.'"
             f"\n\nContext: {context}\n\nQuestion: {question}"
         )
-        payload = json.dumps({
-            "prompt": prompt
-        })
+
+        request_body = {
+            "instances": [
+                {
+                    "prompt": prompt,
+                    "max_tokens": 1000,
+                    "top_k": 1,
+                    "top_p": 1.0,
+                    "temperature": 0.2
+                }
+            ]
+        }
+
         headers = {
-            'Connection': 'keep-alive',
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer 0lXKBW3n6nYYderfIrxE-JeIXjDxKpKBRvOC7Z2anpRuPj_ZTLg8_DJhcCk9-bckksSpWSr1bzf0z_Nw86Klbw=='
+            "Authorization": "Bearer {bearer_token}".format(bearer_token=self.bearer_token),
+            "Content-Type": "application/json"
         }
 
         try:
-            response = requests.post(Config.CLOUD_API_URL, headers=headers, data=payload, timeout=30)
-            return json.loads(response.text)
+            response = requests.post(self.full_url, json=request_body, headers=headers, timeout=30)
+            response = response.json()
+            predictions = response["predictions"]
+            output_text = predictions[0]
+            output_text = output_text.split("Output:", 1)[-1].strip()
+            return json.loads(output_text)
         except requests.exceptions.Timeout:
             return {"error": "The request timed out after 30 seconds."}
         except requests.exceptions.RequestException as e:
